d8924846356e816e4d136ad54809713f2112c951
taloufi <taloufi@iu.edu>
2022-03-05 16:12:24 -0500

    initial commit -- project setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface indexProps {}
+
+const Index: React.FC<indexProps> = ({}) => {
+  return (
+    <div className="text-red-500 font-bold text-3xl flex justify-center items-center h-screen w-screen">
+      Initial hello
+    </div>
+  );
+};
+
+export default Index;
