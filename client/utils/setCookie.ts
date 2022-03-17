587037065b8dc7125400d5a6d54b913af58ecd4f
TK <77863569+tk04@users.noreply.github.com>
2022-03-17 10:55:14 -0400

    Update setCookie.ts

--- client/utils/setCookie.ts
+++ client/utils/setCookie.ts
@@ -3,6 +3,6 @@ import Cookie from "js-cookie";
 export const setToken = (token: string) => {
   Cookie.set("token", token, {
     expires: 30,
-    secure: process.env.NODE_ENV === "production",
+    secure: false,
   });
 };
