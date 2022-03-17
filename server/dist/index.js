a43d6402953a49adf5ee77e2d9147784874a86cb
taloufi <taloufi@iu.edu>
2022-03-16 21:49:55 -0400

    fixed type issues + built for deployment

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,21 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const type_graphql_1 = require("type-graphql");
-const express_1 = __importDefault(require("express"));
-const apollo_server_express_1 = require("apollo-server-express");
-const apollo_server_core_1 = require("apollo-server-core");
-const User_1 = require("./resolvers/User");
 const client_1 = require("@prisma/client");
-require("dotenv/config");
+const apollo_server_core_1 = require("apollo-server-core");
+const apollo_server_express_1 = require("apollo-server-express");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+const express_1 = __importDefault(require("express"));
+const redis_1 = require("redis");
+require("reflect-metadata");
+const type_graphql_1 = require("type-graphql");
 const Tweet_1 = require("./resolvers/Tweet");
-const ioredis_1 = __importDefault(require("ioredis"));
+const User_1 = require("./resolvers/User");
 const main = async () => {
     const app = (0, express_1.default)();
-    const redis = new ioredis_1.default();
+    const redis = (0, redis_1.createClient)({
+        url: process.env.REDIS_URI,
+        password: process.env.REDIS_PASS,
+    });
+    await redis.connect();
     app.use((0, cookie_parser_1.default)(process.env.JWT_SECRET));
     app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
     const prisma = new client_1.PrismaClient();
@@ -31,6 +35,7 @@ const main = async () => {
         app,
         cors: { origin: "http://localhost:3000", credentials: true },
     });
-    app.listen(4000);
+    console.log("PORT: " + process.env.PORT);
+    app.listen(process.env.PORT);
 };
 main();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,26 @@
-import "reflect-metadata";
-import { buildSchema } from "type-graphql";
-import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { UserResolver } from "./resolvers/User";
+import { RedisClientType } from "@node-redis/client";
 import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import { createClient } from "redis";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
 import { TweetResolver } from "./resolvers/Tweet";
-import Redis from "ioredis";
+import { UserResolver } from "./resolvers/User";
+
 const main = async () => {
   const app = express();
-  const redis = new Redis();
+
+  const redis: RedisClientType = createClient({
+    url: process.env.REDIS_URI,
+    password: process.env.REDIS_PASS,
+  });
+  await redis.connect();
+
   app.use(cookieParser(process.env.JWT_SECRET));
   app.use(cors({ origin: "http://localhost:3000", credentials: true }));
   const prisma = new PrismaClient();
@@ -20,13 +28,15 @@ const main = async () => {
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     schema: await buildSchema({ resolvers: [UserResolver, TweetResolver] }),
     context: ({ req, res }) => ({ req, res, prisma, redis }),
+    introspection: true,
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({
     app,
     cors: { origin: "http://localhost:3000", credentials: true },
   });
-  app.listen(4000);
+  console.log("PORT: " + process.env.PORT);
+  app.listen(process.env.PORT);
 };
 
 main();
