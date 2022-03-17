f024a169e2bd589841afd30811ff41331f829441
taloufi <taloufi@iu.edu>
2022-03-17 10:51:43 -0400

    adjust cookie properties

--- client/utils/setCookie.ts
+++ client/utils/setCookie.ts
@@ -1,6 +1,6 @@
 import Cookie from "js-cookie";
 
-export const setToken = (token: string) => {
+export const setToken = (token: string): void => {
   Cookie.set("token", token, {
     expires: 30,
     // secure: process.env.NODE_ENV === "production",
