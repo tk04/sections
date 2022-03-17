03ad84001dc8c20a37d06d6c078adaaaf42fe2ac
taloufi <taloufi@iu.edu>
2022-03-16 21:53:06 -0400

    fixed import issue + built for deployment

--- client/components/ManageTweets.tsx
+++ client/components/ManageTweets.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
   GetMyTweetsDocument,
-  GetTweetsDocument,
   TweetFragmentFragment,
   useDeleteTweetMutation,
   useGetMyTweetsQuery,
