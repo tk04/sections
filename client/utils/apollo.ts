3656372a7746601af3b1f73c2333ee6654b5c2ac
taloufi <taloufi@iu.edu>
2022-03-18 20:27:59 -0400

    testing different cookie options + new httpLink

--- client/utils/apollo.ts
+++ client/utils/apollo.ts
@@ -1,17 +1,27 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import Cookies from "js-cookie";
 
-export const client = new ApolloClient({
+const link = new HttpLink({
   uri:
     process.env.NODE_ENV === "production"
       ? "https://sections-be.herokuapp.com/graphql"
       : "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
   credentials: "include",
+  // headers: {},
+});
+export const client = new ApolloClient({
+  // uri:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://sections-be.herokuapp.com/graphql"
+  //     : "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+  link,
+  // credentials: "include",
 
-  headers: {
-    // AccessControlAllowCredentials: "true",
-    "Access-Control-Allow-Credentials": "true",
-    withCredentials: "true",
-  },
+  // headers: {
+  //   "Access-Control-Allow-Credentials": "true",
+  //   withCredentials: "true",
+  //   mode: "cors",
+  //   credentials: "include",
+  // },
 });
